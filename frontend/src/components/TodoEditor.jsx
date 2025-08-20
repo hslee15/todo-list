58cc4eb08eedf109b3eeacf61b8b3cb52f3a825a
hslee15 <lhs01019@kbu.ac.kr>
2025-08-20 11:01:43 +0900

    up

--- frontend/src/components/TodoEditor.jsx
+++ frontend/src/components/TodoEditor.jsx
@@ -1,13 +1,12 @@
-import React ,{useState}from 'react'
+import React, { useState } from 'react'
 import "./TodoEditor.css"
+const TodoEditor = ({ onCreate }) => {
+  const [text, setText] = useState("")
 
-const TodoEditor = ({onCreate}) => {
-  const [text,setText]=useState("")
-
-  const onSubmit=(e)=>{
+  const onSubmit = (e) => {
     e.preventDefault()
 
-    if(!text.trim()) return
+    if (!text.trim()) return
 
     onCreate(text.trim())
     setText("")
@@ -15,10 +14,13 @@ const TodoEditor = ({onCreate}) => {
   }
   return (
     <form className='TodoEditor' onSubmit={onSubmit}>
-        <input type="text" placeholder='새로운 Todo...' value={text}
-        onChange={(e)=>setText(e.target.value)}
-        />
-        <button type='submit' disabled={!text.trim()}>추가</button>
+      <input
+        type="text"
+        placeholder='새로운 Todo...'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type='submit' disabled={!text.trim()}>추가</button>
     </form>
   )
 }
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import './TodoList.css'
 import TodoItem from './TodoItem'
-const TodoList = ({ todos, updatedChecked, updatedText, onDelete }) => {
+const TodoList = ({todos,onDelete,onUpdateChecked,onUpdateText}) => {
   return (
-    <div className="TodoList">
-      <div className="todos_wrapper">
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo._id}
-            todo={todo}
-            updatedChecked={updatedChecked}
-            updatedText={updatedText}
-            onDelete={onDelete}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
+    <div className='TodoList'>
+        <h4>Todo List 🌱</h4>
+        <input type="text" placeholder='검색어를 입력하세요' />
+        <div className="todos-wrapper">
+          {todos.map((todo,i)=>(
 
+            <TodoItem 
+            key={i} 
+            todo={todo}
+            onUpdateChecked={onUpdateChecked} 
+            onUpdateText={onUpdateText}
+            onDelete={onDelete}/>
+          ))}
+        
+        </div>
+    </div>
+  )
+}
 
 export default TodoList
